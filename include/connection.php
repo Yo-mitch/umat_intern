aa09f03b8f64050a0840c4328b19c817032acb15
Andy-Techy <adjeimitchel03@gmail.com>
2025-05-21 19:47:39 +0000

    commented out the echo message

--- include/connection.php
+++ include/connection.php
@@ -6,6 +6,6 @@
     if (!$connection){
         die("connection failed"). mysqli_connect_error();
     }else{
-        echo "connection successful!";
+        // echo "connection successful!";
     }
 ?>
