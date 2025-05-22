b63b829650b5fdb1f6db412f5816283a59ea9393
Andy-Techy <adjeimitchel03@gmail.com>
2025-05-22 05:34:34 +0000

    added basic auth system checks

--- insert.php
+++ insert.php
@@ -11,18 +11,26 @@ NB: the GET method was used in this program just for practicing purposes. -->
     $firstname = $_POST["firstname"];
     $surname = $_POST["surname"];
     $programmeCode = $_POST["programme-code"];
-    $referernceNumber = $_POST["reference-number"];
-    $domain = $_POST["domain"];
+    $referenceNumber = $_POST["reference-number"];
+    // $domain = $_POST["domain"];
 
     //user authentication
     if (isset($_POST["submit"])) {
         if (empty($firstname) || empty($surname) || empty($programmeCode) || empty($referenceNumber)){
             echo "fill in the blanks";
+        } 
+        // Validate programme code: only letters, 2 characters max
+        elseif (!preg_match('/^[A-Za-z]{1,2}$/', $programmeCode)) {
+            echo "Programme code must be letters only and not more than 2 characters.";
+        }
+        // Validate reference number: only digits, max length 10
+        elseif (!preg_match('/^\d{1,10}$/', $referenceNumber)) {
+            echo "Reference number must be digits only and not more than 10 digits.";
         }
         else{
             //code snippet to push data into the database
             // insert into table (name of table columns created) where (the respective values for the tables)
-            $query = " INSERT INTO  login (first_name,  surname, programme_code, reference_number, domain) VALUES ('$firstname', '$surname', '$programmeCode', '$referenceNumber' , '$domain')";
+            $query = " INSERT INTO  login (first_name,  surname, programme_code, reference_number) VALUES ('$firstname', '$surname', '$programmeCode', '$referenceNumber' )";
 
             //real pushing is done here
             $result = mysqli_query($connection, $query);
