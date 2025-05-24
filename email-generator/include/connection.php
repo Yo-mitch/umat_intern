a7fa38358f6e99b421518e904f605826262821e3
Andy-Techy <adjeimitchel03@gmail.com>
2025-05-24 12:15:59 +0000

    modified connection.php

--- email-generator/include/connection.php
+++ email-generator/include/connection.php
@@ -1,7 +1,7 @@
-<?php
-    //this snippet holds the connection to the database
-    $connection = mysqli_connect("localhost", "intern", "Intern@UMaT", "interndb");
+<!-- this snippet holds the connection to the database -->
 
+<?php
+    $connection = mysqli_connect("localhost", "intern", "Intern@UMaT", "interndb");
     //check if the connection is successful
     if (!$connection){
         die("connection failed"). mysqli_connect_error();
