5f712a45da1b0f607cf3badf74af8f7dbbca1cf1
Andy-Techy <adjeimitchel03@gmail.com>
2025-05-25 21:04:50 +0000

    modified insert.php

--- email-generator/insert.php
+++ email-generator/insert.php
@@ -19,10 +19,6 @@ NB: the GET method was used in this program just for practicing purposes. -->
         if (empty($firstname) || empty($surname) || empty($programmeCode) || empty($referenceNumber)){
             echo "fill in the blanks";
         } 
-        // Validate programme code: only letters, 2 characters max
-        elseif (!preg_match('/^[A-Za-z]{1,2}$/', $programmeCode)) {
-            echo "Programme code must be letters only and not more than 2 characters.";
-        }
         // Validate reference number: only digits, max length 10
         elseif (!preg_match('/^\d{1,10}$/', $referenceNumber)) {
             echo "Reference number must be digits only and not more than 10 digits.";
@@ -30,7 +26,7 @@ NB: the GET method was used in this program just for practicing purposes. -->
         else{
             //code snippet to push data into the database
             // insert into table (name of table columns created) where (the respective values for the tables)
-            $query = " INSERT INTO  login (first_name,  surname, programme_code, reference_number) VALUES ('$firstname', '$surname', '$programmeCode', '$referenceNumber' )";
+            $query = " INSERT INTO  login (first_name,  surname, programme, reference_number) VALUES ('$firstname', '$surname', '$programmeCode', '$referenceNumber' )";
 
             //real pushing is done here
             $result = mysqli_query($connection, $query);
