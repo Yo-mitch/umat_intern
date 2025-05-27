3435b97931ab752b2233e26a37860daba8283141
Andy-Techy <adjeimitchel03@gmail.com>
2025-05-27 00:24:54 +0000

    modified

--- email-generator/view.php
+++ email-generator/view.php
@@ -6,7 +6,7 @@ require_once("include/connection.php");
 
 // 1. arrange in descending order and
 // 2. make the limit one to just retrieve the latest stored data
-$query = "SELECT * FROM login ORDER BY id DESC LIMIT 1";
+$query = " SELECT * FROM login ORDER BY id DESC LIMIT 1 ";
 
 //save the result
 $result = mysqli_query($connection, $query);
@@ -15,10 +15,10 @@ $result = mysqli_query($connection, $query);
 // and saves it into the row
 if ($row = mysqli_fetch_assoc($result)) {
     //this stores the row of the first name into the firstname variable
-    $firstname = $row['first_name'];
-    $surname = $row['surname'];
-    $programmeCode = $row['programme_code'];
-    $referenceNumber = $row['reference_number'];
+    $firstname = $row["first_name"];
+    $surname = $row["surname"];
+    $programmeCode = $row["programme"];
+    $referenceNumber = $row["reference_number"];
     // $domain = $row['domain'];
 
     // *****************Generate email****************
@@ -31,8 +31,25 @@ if ($row = mysqli_fetch_assoc($result)) {
     // extracts the last four digits starting from the end, negative means would always begin from the end
     $lastFourDigits = substr($referenceNumber, -4);
 
+    // getting the programme code between brackets
+    $startChar = '(';
+    $endChar = ')';
+
+    $lower = strtolower($programmeCode);
+    $startIndex = strpos($lower, strtolower($startChar));
+    $endIndex = strpos($lower, strtolower($endChar));
+
+    $extractingProgrammeCode = '';
+
+    if ($startIndex === false || $endIndex === false || $endIndex <= $startIndex) {
+        $extractingProgrammeCode = "Start or end character not found, or in wrong order.";
+    } else {
+        $result = substr($lower, $startIndex + 1, $endIndex - $startIndex - 1);
+        $extractingProgrammeCode = $result;
+    }
+
     //the email format.
-    $email = strtolower($programmeCode). "-". $firstInitial . strtolower($surname) . $lastFourDigits . "@st.umat.edu.gh";
+    $email = strtolower($extractingProgrammeCode). "-". $firstInitial . strtolower($surname) . $lastFourDigits . "@st.umat.edu.gh";
 } else {
     $email = "No record found.";
 }
@@ -45,12 +62,12 @@ if ($row = mysqli_fetch_assoc($result)) {
     <title>Email Generated</title>
 </head>
 <body>
-    <h2>Sign-up Successful!</h2>
+    <!-- <h2>Sign-up Successful!</h2> -->
     <!-- check if row is declared -->
     <?php if (isset($row)): ?>
         <h3> Hurray!🚀 </h3>
         <p> Your Email is : <p><strong><?php echo htmlspecialchars($email); ?></strong></p> </p>
-        <p><a href="resetpassword.php">Click here to set your password</a></p>
+        <p><a href="resetpassword.php">Click here to reset your password</a></p>
     <?php else: ?>
         <p><?php echo $email; ?></p>
         <!-- a link to set new password -->
